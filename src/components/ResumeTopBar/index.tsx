91a4481f16337d48d7f5b5c940ff544c97daabbd
Thiago Vinícius <80261904+ubyss@users.noreply.github.com>
2023-05-23 14:36:35 -0300

    Fix: header layout

--- src/components/ResumeTopBar/index.tsx
+++ src/components/ResumeTopBar/index.tsx
@@ -3,8 +3,10 @@ import React from 'react'
 const ResumeTopBar = () => {
   return (
     <div className='ResumeTopBar'>
+      <div className="ResumeTopBar__text">
         <h2>Resumo do pedido</h2>
         <button>Continuar comprando</button>
+      </div>
     </div>
   )
 }
